a1356d979cbdf29e813eecb81edac51a96cffa19
marufjon <marufjon@192.168.12.101>
2018-09-05 16:57:09 -0400

    added junit report option

--- src/test/java/com/prestashop/runners/CukesRunner.java
+++ src/test/java/com/prestashop/runners/CukesRunner.java
@@ -10,11 +10,12 @@ import cucumber.api.junit.Cucumber;
 		plugin = {
 				"pretty",
 				"html:target/default-cucumber-reports", //generates every time we run the cukes runner
-				"json:target/cucumber.json"
+				"json:target/cucumber.json",
+				"junit:target/cucumber.xml"
 		
 		},
 		
-		tags="@login",
+		tags="@temp",
 		features= {"src/test/resources/com/prestashop/features", 
 				"src/test/resources/com/ct_bnb/features"}, 
 		glue= {"com/prestashop/step_definitions", "com/ctbnb/step_definitions" }
