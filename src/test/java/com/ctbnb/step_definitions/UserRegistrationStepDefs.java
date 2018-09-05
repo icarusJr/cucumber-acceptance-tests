ee5c69f56db6605e1cb1670e341489325e658861
github@cybertekschool.com <github@cybertekschool.com>
2018-09-04 20:59:12 -0400

    create user with exsting email test

--- src/test/java/com/ctbnb/step_definitions/UserRegistrationStepDefs.java
+++ src/test/java/com/ctbnb/step_definitions/UserRegistrationStepDefs.java
@@ -1,10 +1,13 @@
 package com.ctbnb.step_definitions;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import com.prestashop.utilities.DBUtils;
 import com.prestashop.utilities.RestUtils;
 import com.prestashop.utilities.RestUtils.UserType;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -26,13 +29,9 @@ public class UserRegistrationStepDefs {
 	public void i_try_to_register_a_new_user() {
 		responce = RestAssured.given().header("Authorization", RestUtils.token)
 				// .param("first-name", "Johnny")
-				.param("last-name", "Cage").param("email", "johnnycage@gmail.com")
-				.param("password", "subzerobest")
-				.param("role", "student-team-member")
-				.param("batch-number", "8")
-				.param("team-name", "CodeHunters")
-				.param("campus-location", "VA")
-				.when().post(RestAssured.baseURI + "/api/students/student");
+				.param("last-name", "Cage").param("email", "johnnycage@gmail.com").param("password", "subzerobest")
+				.param("role", "student-team-member").param("batch-number", "8").param("team-name", "CodeHunters")
+				.param("campus-location", "VA").when().post(RestAssured.baseURI + "/api/students/student");
 	}
 
 	@Then("system should return only teacher can register message")
@@ -49,7 +48,39 @@ public class UserRegistrationStepDefs {
 	@Then("the teacher should be authorised to add users")
 	public void the_teacher_should_be_authorised_to_add_users() {
 		responce.then().statusCode(422);
-		assertEquals("pay attentisdfsdon to how you specifying first-name in the query.", responce.body().asString());
+		assertEquals("pay attention to how you specifying first-name in the query.", responce.body().asString());
+
+	}
+
+	@When("I try to register a new user with existing email")
+	public void i_try_to_register_a_new_user_with_existing_email() {
+		// go tpo database and get a existing user
+		//  using the user information from databse, create new querypath
+		// post
+		
+		String sql = "select  email from users\n" + 
+				"where email is not null\n" + 
+				" limit 1;";
+		String email = (String) DBUtils.getCellValue(sql);
+		assertNotNull(email);
+		
+		responce = RestAssured.given().header("Authorization", RestUtils.token)
+				 .param("first-name", "Johnny")
+				.param("last-name", "Cage")
+				.param("email", email)
+				.param("password", "subzerxobest")
+				.param("role", "student-team-member")
+				.param("batch-number", "8")
+				.param("team-name", "CodeHunters")
+				.param("campus-location", "VA").when().post(RestAssured.baseURI + "/api/students/student");
+
+	
+	}
+
+	@Then("user with same email exists message should be returned")
+	public void user_with_same_email_exists_message_should_be_returned() {
+		responce.then().statusCode(422);
+		assertEquals("user with the email: johnnycage@gmail.com is already exist.", responce.body().asString());
 
 	}
 
--- src/test/resources/com/ct_bnb/features/user_registration.feature
+++ src/test/resources/com/ct_bnb/features/user_registration.feature
@@ -12,9 +12,20 @@ Scenario: Permissions verification: team member
 	When I try to register a new user 
 	Then system should return only teacher can register message
 
-@ws	@temp 	
+@ws	 	
 Scenario: Permissions verification: teacher 
 	Given I am logged reservation api as teacher 
 	When I try to register a new user 
 	Then the teacher should be authorised to add users
+
+@ws	@temp @db	
+Scenario: Verify existing user email 
+	Given I am logged reservation api as teacher 
+	When I try to register a new user with existing email
+	Then user with same email exists message should be returned
+	
+	
+	
+	
+	
 	
